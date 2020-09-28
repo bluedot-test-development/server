b0e2e6b6cacaa4a8e46a98c8477cd902e0639587
jongwon <54282927+loosie@users.noreply.github.com>
2020-09-29 01:29:25 +0900

    09.28 인가 작업1
    1)요청받을때마다 토큰 분석 -> BasicAuthenticationFilter 사용 ///

--- src/main/java/develop/bluedot/server/controller/UserController.java
+++ src/main/java/develop/bluedot/server/controller/UserController.java
@@ -1,20 +1,16 @@
 package develop.bluedot.server.controller;
 
 import develop.bluedot.server.entity.User;
-import develop.bluedot.server.network.Header;
 import develop.bluedot.server.network.request.UserApiRequest;
 import develop.bluedot.server.network.response.PostApiResponse;
 import develop.bluedot.server.network.response.UserApiResponse;
 import develop.bluedot.server.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @Slf4j
@@ -26,7 +22,7 @@ public class UserController extends CrudController<UserApiRequest, UserApiRespon
     private UserService userService;
 
     /**
-     * 페이징
+     * 페이징처리
     @GetMapping("/page")
     public Header<List<UserApiResponse>> search(
             @PageableDefault(sort="id",direction = Sort.Direction.ASC,size=10)
@@ -34,7 +30,6 @@ public class UserController extends CrudController<UserApiRequest, UserApiRespon
         return userService.search(pageable);
     }
 */
-
     @GetMapping("/artist")
     public List<UserApiResponse> getArtist(){
         return userService.getArtist();
